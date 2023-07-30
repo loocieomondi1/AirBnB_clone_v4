8f1ac54b1b47bff8f62fc64abdb616df2b160546
bmn44100 <njerumurimi@gmail.com>
2023-07-30 12:45:30 -0700

    pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -31,12 +31,13 @@ class DBStorage:
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')
+        HBNB_ENV = getenv('HBNB_ENV', "none")
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(HBNB_MYSQL_USER,
                                              HBNB_MYSQL_PWD,
                                              HBNB_MYSQL_HOST,
-                                             HBNB_MYSQL_DB))
+                                             HBNB_MYSQL_DB),
+                                      pool_pre_ping=True)
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
