f808714ee5c33008d84c52f3428a325c4cf7c280
loocieomondi <loocieomondi@gmail.com>
2023-07-30 06:15:11 +0000

    web

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""Starts a Flask web application
 """
+starts a Flask web application
+"""
+
 from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app = Flask(__name__)
-
-    @app.route('/', strict_slashes=False)
-    def index():
-        """Display 'Hello HBNB!'
-        """
-        return 'Hello HBNB!'
-
-    @app.route('/hbnb', strict_slashes=False)
-    def hbnb():
-        """Display 'HBNB'
-        """
-        return 'HBNB'
-
-    @app.route('/c/<text>', strict_slashes=False)
-    def c(text):
-        """Display “C ” followed by the value of
-        the text variable (replace underscore _
-        symbols with a space)
-        """
-        return 'C ' + text.replace('_', ' ')
-
-    app.run('0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,51 +1,47 @@
 #!/usr/bin/python3
-"""Starts a Flask web application
 """
-from flask import Flask
-from flask import render_template
+starts a Flask web application
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbersandtemplates(n):
+    """display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
-    app = Flask(__name__)
-
-    @app.route('/', strict_slashes=False)
-    def index():
-        """Display 'Hello HBNB!'
-        """
-        return 'Hello HBNB!'
-
-    @app.route('/hbnb', strict_slashes=False)
-    def hbnb():
-        """Display 'HBNB'
-        """
-        return 'HBNB'
-
-    @app.route('/c/<text>', strict_slashes=False)
-    def c(text):
-        """Display “C ” followed by the value of
-        the text variable (replace underscore _
-        symbols with a space)
-        """
-        return 'C ' + text.replace('_', ' ')
-
-    @app.route('/python/')
-    @app.route('/python/<text>', strict_slashes=False)
-    def python(text="is cool"):
-        """Display “Python ”, followed by the value of
-        the text variable (replace underscore _
-        symbols with a space )
-        """
-        return 'Python ' + text.replace('_', ' ')
-
-    @app.route('/number/<int:n>', strict_slashes=False)
-    def number(n):
-        """Display “n is a number” only if n is an integer
-        """
-        return str(n) + ' is a number'
-
-    @app.route('/number_template/<int:n>', strict_slashes=False)
-    def number_template(n):
-        """Display a HTML page only if n is an integer
-        """
-        return render_template('5-number.html', n=n)
-
-    app.run('0.0.0.0')
+    app.run(host='0.0.0.0', port='5000')
